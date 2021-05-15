816b60ee941697f639804391f46021fae732c849
Diego Guerrero <diego.guerrero02@uptc.edu.co>
2021-05-15 15:06:56 -0500

    no sé

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var http = require('http'),
+    path = require('path'),
+    os = require('os'),
+    fs = require('fs');
+const Busboy = require('busboy');
+
 const PORT = process.argv[2];
 
 const app = express()
@@ -8,7 +14,22 @@ app.use(bodyParser.json())
 
 app.post('/fileupload', (req, res) => {
     console.log(req)
-    res.sendStatus(200);
+    http.createServer(function(req, res) {
+        if (req.method === 'POST') {
+          var busboy = new Busboy({ headers: req.headers });
+          busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+            var saveTo = path.join(os.tmpDir(), path.basename(fieldname));
+            file.pipe(fs.createWriteStream(saveTo));
+          });
+          busboy.on('finish', function() {
+            res.writeHead(200, { 'Connection': 'close' });
+            res.end("That's all folks!");
+          });
+          return req.pipe(busboy);
+        }
+        res.writeHead(404);
+        res.end();
+    })
 })
 
 app.listen(PORT, () => {
